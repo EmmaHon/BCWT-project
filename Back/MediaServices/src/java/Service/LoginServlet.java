bb987d6bcc7f95bc946ff60a25156537f9d91682
Daniel Vahla <vahla.daniel@gmail.com>
2017-12-11 13:36:02 +0200

    servlet added

--- Back/MediaServices/src/java/Service/LoginServlet.java
+++ Back/MediaServices/src/java/Service/LoginServlet.java
@@ -5,9 +5,14 @@
  */
 package Service;
 
+import Bean.SessionBean;
+import Entity.Users;
 import java.io.IOException;
 import java.io.PrintWriter;
+import javax.ejb.EJB;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -16,61 +21,36 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author danie
  */
+@WebServlet(name="Login", urlPatterns = {"/loginServlet"})
 public class LoginServlet extends HttpServlet {
+    
+    @EJB
+    private SessionBean sb;
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet LoginServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet LoginServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        response.addHeader("Access-Control-Allow-Origin", "*");
+        response.addHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, HEAD");
+        
+        String email = request.getParameter("email");
+        String pwd = request.getParameter("password");
+        Users possibleUser;
+        
+        try{
+        possibleUser = sb.loginUser(email, pwd);}
+        catch(Exception pokemon){
+        possibleUser = null;
+        }
+        
+        if(possibleUser!=null){
+            response.setStatus(200);
+            Cookie cookie;
+            cookie = new Cookie("loginCookie",Integer.toString(sb.getToken(email, pwd)));
+            response.addCookie(cookie);
+        }else{
+            response.setStatus(418);
+        }
     }
 
     /**
